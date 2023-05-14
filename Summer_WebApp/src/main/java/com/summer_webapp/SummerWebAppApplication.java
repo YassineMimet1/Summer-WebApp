dd7fe3649a7e843c6f4088aaa54d25cd3d714ee6
souhailelhayani <souhail.200308@gmail.com>
2023-05-14 18:10:34 -0400

    first commit

---
+++ Summer_WebApp/src/main/java/com/summer_webapp/SummerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.summer_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerWebAppApplication.class, args);
+	}
+
+}
---
+++ Summer_WebApp/src/test/java/com/summer_webapp/SummerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
